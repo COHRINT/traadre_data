d436a7b74a09eb906c23d52ea82db0e4f1905e15
jamc3951 <jamc3951@colorado.edu>
2020-01-08 16:17:34 -0700

    information + option

--- mplwidget.py
+++ mplwidget.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python2.7
 
 # Imports
+import rospy
+import math
 from PyQt5 import QtWidgets
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
 import matplotlib
 import matplotlib.pyplot as plt
+from traadre_msgs.srv import *
+from traadre_msgs.msg import *
 
-
-
-
-class MplCanvas(Canvas):
-	def __init__(self):
-		def onclick(event):
-			'''print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-				('double' if event.dblclick else 'single', event.button,
-				 event.x, event.y, event.xdata, event.ydata))'''
-			try:
-				self.span.remove()
-			except:
-				pass
-			self.span = event.canvas.ax.axvspan(event.x, event.x+10, color='red', alpha=0.5)
-			event.canvas.draw()
-
-
-		# Matplotlib canvas class to create figure
-
-
-
-		self.fig = Figure()
-		self.ax = self.fig.add_subplot(111)
-		Canvas.__init__(self, self.fig)
-		Canvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-		Canvas.updateGeometry(self)
-
-		cid = self.fig.canvas.mpl_connect('button_press_event', onclick)
 
 # Matplotlib widget
 class MplWidget(QtWidgets.QWidget):
@@ -44,5 +20,76 @@ class MplWidget(QtWidgets.QWidget):
 		self.vbl = QtWidgets.QVBoxLayout()         # Set box for plotting
 		self.vbl.addWidget(self.canvas)
 		self.setLayout(self.vbl)
+
+
+
+
+class MplCanvas(Canvas):
+		def __init__(self):
+
+			def onclick(event):
+				msg = OptionSelect()
+				'''print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+					('double' if event.dblclick else 'single', event.button,
+					 event.x, event.y, event.xdata, event.ydata))'''
+				
+				self.rewards = self.getRewards_client((self._goal))
+				max_reward = max(self.rewards)
+				width = max_reward*0.16
+				location = 0
+				if event.xdata:
+					ax_min, ax_max = event.canvas.ax.get_xlim()
+					location = math.floor(event.xdata/width) +1 
+
+				try:
+					self.span.remove()
+				except:
+					pass
+				if location < 6:
+					self.span = event.canvas.ax.axvspan(0, location*width, color='red', alpha=0.5)
+				event.canvas.draw()
+
+				msg.option = int(location-1)
+				self.option_pub.publish(msg)
+
+			self.fig = Figure()
+			self.ax = self.fig.add_subplot(111)
+			Canvas.__init__(self, self.fig)
+			# Matplotlib canvas class to create figure
+
+			Canvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+			Canvas.updateGeometry(self)
+
+
+			cid = self.fig.canvas.mpl_connect('button_press_event', onclick)
+			self.goal_sub = rospy.Subscriber('current_goal', NamedGoal, self.goal_cb)
+			self.option_pub = rospy.Publisher('option', OptionSelect, queue_size=10)
+
+		def goal_cb(self, msg):
+			 #Resolve the odometry to a screen coordinate for display
+
+			worldX = msg.pose.x
+			worldY = msg.pose.y
+
+			print 'Got Goal at: ' + str(worldX) + ',' + str(worldY)
+
+			self._goal = msg.id
+				#self.goals_changed.emit()
+
+
+
+
+
+		def getRewards_client(self, id):
+			try:
+				goal = rospy.ServiceProxy('/policy/policy_server/GetMCSims', GetMCSims)
+				response = goal(id)
+
+				return response.rewards
+			except rospy.ServiceException, e:
+				print "Service call failed: %s"%e
+
+
+
 
 
